934d732d18c060990e9586bcf5b6d7b0563556d8
Dmitry Ponyatov <dponyatov@gmail.com>
2016-02-21 11:18:39 +0400

    v algolic syntax
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cpp.cpp
+++ cpp.cpp
@@ -1,14 +1,21 @@
 #include "hpp.hpp"
 #define YYERR "\n\n"<<yylineno<<":"<<msg<<"["<<yytext<<"]\n\n"
 void yyerror(string msg) { cout<<YYERR; cerr<<YYERR; exit(-1); }
-int main() { glob_init(); yyparse(); module->dump(); return 0; }
+int main() { glob_init(); yyparse(); cout<<module->dump(); return 0; }
 
 Sym::Sym(string T,string V) { tag=T; val=V; env=&glob; }
 Sym::Sym(string V):Sym("",V) {}
 void Sym::push(Sym*o) { nest.push_back(o); }
 
 string Sym::tagval() { return "<"+tag+":"+val+">"; }
-string Sym::tagstr() { return "<"+tag+":'"+val+"'>"; }
+string Sym::tagstr() { string S = "<"+tag+":'";
+	for (int i=0,n=val.length();i<n;i++)
+		switch (val[i]) {
+			case '\t': S+="\\t"; break;
+			case '\n': S+="\\n"; break;
+			default: S+=val[i];
+		}
+	return S+"'>"; }
 string Sym::pad(int n) { string S; for (int i=0;i<n;i++) S+='\t'; return S; }
 string Sym::dump(int depth) { string S = "\n"+pad(depth)+tagval();
 	for (auto it=nest.begin(),e=nest.end();it!=e;it++)
